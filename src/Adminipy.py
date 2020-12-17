e1d92219b76b6a360d6d796b72b928b068597674
kristhaj <kristhaj@stud.ntnu.no>
2020-12-17 17:03:12 +0100

    add writing low data quality file, calculating match rating and begin lookup of missing data in member registry

--- src/Adminipy.py
+++ src/Adminipy.py
@@ -8,14 +8,15 @@ import pandas as pd
 # NOTE: batch 1 is differently formated than the others
 def LoadDataFrames(path_call, path_members, qualifier_path):
     df_call = pd.read_excel(path_call, usecols='A,C,H:J,T:V')
-    df_data = pd.read_excel(path_members, usecols='B,C,H,M')
+    df_data = pd.read_excel(path_members, usecols='A,D:H,J')
     df_ql = pd.read_excel(qualifier_path, sheet_name='Tabell', usecols='A,B,H')
+    df_org = pd.read_excel(path_members, sheet_name='Club info', usecols='A,B,D')
 
-    return df_call, df_data, df_ql
+    return df_call, df_data, df_ql, df_org
 
 # Cross reference the list names with member data
 # returns indices of people identified as admins
-def IdentifyAdmins(names, data, ql):
+def IdentifyAdmins(names, data, ql, org):
     #Identifier in col A in ql
     batchID = 'Pulje 2'
     rel_clubs = getRelevantClubs(batchID, ql)
@@ -24,9 +25,82 @@ def IdentifyAdmins(names, data, ql):
     #print degree of match
     getMatchRate(rel_clubs, callees, batchID)
 
+    club_indices = list(rel_clubs.keys())
+    callee_indices = list(callees.keys())
+    matched_indices = []
+    for i in callee_indices:
+        for j in club_indices:
+            if callees[i][0].replace(' ', '').lower() == rel_clubs[j][0].replace(' ', '').lower():
+                matched_indices.append(i)
+    print(f'Identified admins at indices: {matched_indices} \n\n{len(matched_indices)} in total.')
 
+    admin_info = getAdminInfo(callees, matched_indices, data, org)
+    pass
+#Look up missing information based upon 
+def getAdminInfo(callees, matched_indices, df, org):
+    missing_data = []
+    for k in matched_indices:
+        for info in callees[k]:
+            if type(info) is float and k not in missing_data:
+                missing_data.append(k)
+                #print(f'{callees[k]} does not have sufficient data.')
+    print(f'{len(missing_data)} clubs have poor data quality.\n-----')
     
-        
+    #Fill in missing names and contact info
+    callees = getMissingData(callees, missing_data, df, org)
+
+    #Write_Temp_Contacts(callees)
+
+#Write temporary contact information to file
+def Write_Temp_Contacts(dictionary):
+    data = {'Header': ['Klubbnavn', 'Kontaktperson', 'Mobil', 'Epost']}
+    data.update(dictionary)
+    temp_df = pd.DataFrame.from_dict(data, orient='index')
+    with pd.ExcelWriter('pyxcel/files/admin/delvis_kontaktinformasjon3.xlsx') as writer:
+            temp_df.to_excel(writer, index=False, header=False)
+
+#TODO: do lookup in df with built in method
+def getMissingData(callees, indices, df, org):
+    print(f'======\nGetting missing contact information...')
+    for i in indices:
+        identifier = None
+        missing = []
+        print(f'\nHandling {callees[i][0]}\n-----')
+        for info in callees[i]:
+            #ignore club name
+            if callees[i].index(info) != 0:
+                #assign identifier if not assigned
+                if type(info) != float and identifier == None:
+                    if callees[i].index(info) == 1:
+                        identifier = ['Name', info]
+                    elif callees[i].index(info) == 2:
+                        identifier = ['Mobile', info]
+                    elif callees[i].index(info) == 3:
+                        identifier = ['Email', info]
+                    print(f'Setting {identifier} as identifier.')
+                #identify which information if missing
+                else:
+                    if type(info) == float:
+                        if callees[i].index(info) == 1:
+                            missing.append('Name')
+                        elif callees[i].index(info) == 2:
+                            missing.append('Mobile')
+                        elif callees[i].index(info) == 3:
+                            missing.append('Email')
+        print(f'{missing} requires lookup in member registry.')
+        #look up missing information by matching identifier
+        if identifier[0] == 'Name':
+            for index in range(df.shape[0]):
+                comp_name = df.Firstname[index] + df.Lastname[index]
+                if identifier[1].replace(' ', '').lower() == comp_name.replace(' ', '').lower():
+                    callees[i].append(index)
+                    break
+        elif identifier[0] == 'Mobile':
+            #for index in range(df.shape[0]):
+            pass
+            
+
+
     pass
 
 def getMatchRate(rel_clubs, callees, batchID):
@@ -51,9 +125,9 @@ def getMatchRate(rel_clubs, callees, batchID):
     for match in matched:
         if match in missing:
             del(missing[missing.index(match)])
-    print(f'Clubs with potential admins: {len(matched)}\nClubs without match: {len(missing)}\nClubs not imported or not matched: {len(surplus)}\nPotential admin match rate: {round(len(matched)/needed_count, 3)}')
-    #for m in missing:
-        #print(f'-----\nClub requiring manual input: {m}')
+    print(f'Clubs with potential admins: {len(matched)}\nClubs without match: {len(missing)}\nClubs not imported or not matched: {len(surplus)}\nPotential admin match rate: {round(len(matched)/needed_count, 3)}\n------')
+    for m in missing:
+        print(f'Club requiring manual input: {m}')
     print('=====')
 
 
@@ -112,9 +186,9 @@ def Main():
     qualifier_path = 'pyxcel/files/admin/migreringsoversikt.xlsx'
     destination_path = 'pyxcel/files/admin/testo.xlsx'
 
-    callees, data, ql = LoadDataFrames(call_path, member_data_path, qualifier_path)
+    callees, data, ql, org = LoadDataFrames(call_path, member_data_path, qualifier_path)
 
-    IdentifyAdmins(callees, data, ql)
+    IdentifyAdmins(callees, data, ql, org)
 
 
 Main()
