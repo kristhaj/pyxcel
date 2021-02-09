5d47f48400b42ace5ab1812163563a7d9a514c91
Kristoffer Hajem <hajemkristoffer@gmail.com>
2021-02-09 10:37:08 +0100

    remove resolved TODO

--- src/Migrator/Load.py
+++ src/Migrator/Load.py
@@ -7,10 +7,11 @@ class Load:
 
     # Read a single specified file
     def One(self, path):
-        # TODO: fix encoding error (can't read utf-8)
-        df = pd.read_csv(path, parse_dates=True, infer_datetime_format=True, dayfirst=True)
-        
-        return None
+        print(f'Loading Data Basis from {path.split("/")[-1]}')
+        df = pd.read_csv(path, encoding='ISO-8859-1', sep=';', parse_dates=True, infer_datetime_format=True, dayfirst=True)
+        data = df.to_dict()
+        print(f'Finished Loading Data.\n-----')
+        return data
 
     # Read many specified files
     def Many(self, paths):
