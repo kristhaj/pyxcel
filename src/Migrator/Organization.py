0c085ccbc51bf74894113b29c0f0b222ff8ea138
Kristoffer Hajem <hajemkristoffer@gmail.com>
2021-02-25 16:25:45 +0100

    edit printing

--- src/Migrator/Organization.py
+++ src/Migrator/Organization.py
@@ -4,9 +4,11 @@ class Organization:
 
     # Get data from specified organizations or from all
     def Get_Data(self, data, sheet):
+        print(f'Processing Club Info for:')
         for key in list(data.keys()):
+            print(f'{data[key][0]}....')
             sheet['Name of club'].update({len(sheet['Name of club']): data[key][0]})
             sheet['Offical name of club'].update({len(sheet['Offical name of club']): data[key][0]})
             sheet['NIF ClubID'].update({len(sheet['NIF ClubID']): key})
-
+        print('All relevant Club Info Processed.\n===')
         return sheet
