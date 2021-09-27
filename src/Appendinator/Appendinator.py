1c9408d7bcd2aeb9917b071d43aa5dd05a19374a
Kristoffer Hajem <hajemkristoffer@gmail.com>
2021-09-27 15:22:47 +0200

    add temporary pre/post KA handling

--- src/Appendinator/Appendinator.py
+++ src/Appendinator/Appendinator.py
@@ -57,8 +57,10 @@ class Appendinator:
         return pid_dict
 
     def Main(self):
-        output_ID = self.Handle_OutputIDs(self.kao_meta)
-        personIDs = self.Get_PersonIDs(self.kao_dir)
+        postKA = False
+        if postKA:
+            output_ID = self.Handle_OutputIDs(self.kao_meta)
+            personIDs = self.Get_PersonIDs(self.kao_dir)
         df = pd.read_excel(self.format_path, sheet_name=None, skiprows=1, keep_default_na=False)
         print(f'Reading data from {self.data_dir} ...')
         files = os.listdir(self.data_dir)
@@ -94,28 +96,29 @@ class Appendinator:
                             tf = data[key]['Treningsavgift navn'][last_row]
                             if tf not in list(data['Training fee']['Navn på Treningsvgift'].values):
                                 print(f'{current_org}: Bad Training Fee at {last_row}, product name: {tf}')
-                            # check for existance of output data for current org
-                            if current_org in list(output_ID.keys()):
-                                oid = output_ID[current_org]
-                                if oid in list(personIDs.keys()):
-                                    org_data = personIDs[oid]
-                                    num_pid= len(org_data)
-                                    # set PersonID for member if available in output file
-                                    if last_row < num_pid:
-                                        data[key]['NIF ID'][last_row] = org_data[last_row][0]
-                                    #check for erronious last names, and attempt correction (based upon rules for last name in Folkereg.)
-                                    lastname = data[key].Etternavn.values[last_row]
-                                    lastname_count = len(lastname.split())
-                                    real_lastname = org_data[last_row][2]
-                                    real_firstname = org_data[last_row][1]
-                                    if lastname_count > 1 and lastname != real_lastname:
-                                        print(f'{current_org}: Bad name at {last_row}, old: {lastname}, new: {real_lastname}')
-                                        data[key]['Fornavn- og middelnavn'].values[last_row] = real_firstname
-                                        data[key].Etternavn.values[last_row] = real_lastname
-                                        bad_data_count += 1
-                            # log clubs without indentifiable output from KA
-                            else:
-                                missing_output.update({current_org: last_row})
+                            if postKA:
+                                # check for existance of output data for current org
+                                if current_org in list(output_ID.keys()):
+                                    oid = output_ID[current_org]
+                                    if oid in list(personIDs.keys()):
+                                        org_data = personIDs[oid]
+                                        num_pid= len(org_data)
+                                        # set PersonID for member if available in output file
+                                        if last_row < num_pid:
+                                            data[key]['NIF ID'][last_row] = org_data[last_row][0]
+                                        #check for erronious last names, and attempt correction (based upon rules for last name in Folkereg.)
+                                        lastname = data[key].Etternavn.values[last_row]
+                                        lastname_count = len(lastname.split())
+                                        real_lastname = org_data[last_row][2]
+                                        real_firstname = org_data[last_row][1]
+                                        if lastname_count > 1 and lastname != real_lastname:
+                                            print(f'{current_org}: Bad name at {last_row}, old: {lastname}, new: {real_lastname}')
+                                            data[key]['Fornavn- og middelnavn'].values[last_row] = real_firstname
+                                            data[key].Etternavn.values[last_row] = real_lastname
+                                            bad_data_count += 1
+                                # log clubs without indentifiable output from KA
+                                else:
+                                    missing_output.update({current_org: last_row})
                         elif key == 'Training fee':
                             #set TF duration to 1 if missing
                             if data[key]['Varighet (putt inn heltall)'][last_row] == '':
@@ -163,7 +166,8 @@ class Appendinator:
             print(f'Processed {files_handled} out of {len(files)} files.\n')
             
         print(f'---\nFinished Processing All Data\n===\n')
-        print(f'Missing  output for thise orgIDs: {missing_output}\n')
+        if len(list(missing_output.keys())) >= 1:
+            print(f'Missing  output for thise orgIDs: {missing_output}\n')
         print(f'Registered bad data at {bad_data}')
         print(f'Writing data to target: {self.target_path} ...')
         # Write the collated and formated data to a new file
