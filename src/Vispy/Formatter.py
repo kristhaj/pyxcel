10403832cf763877cd3006b35f071f5c892d685f
kristhaj <hajemkristoffer@gmail.com>
2022-03-15 14:17:48 +0100

    setup formatter to handle given formats

---
+++ src/Vispy/Formatter.py
@@ -0,0 +1,9 @@
+# Format the processed data based on the given template
+
+class Formatter:
+
+
+    # Format data into Visma importable format
+    def Visma_Format(self, data, template):
+
+        pass
