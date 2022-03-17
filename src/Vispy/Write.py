a500b57803928cefa956886f5f3212411163b86b
kristhaj <hajemkristoffer@gmail.com>
2022-03-17 12:31:18 +0100

    add writing of output

--- src/Vispy/Write.py
+++ src/Vispy/Write.py
@@ -1,8 +1,18 @@
 # Write processed data into final output format defined by the given template
 
+import pandas as pd
+
 class Write:
 
     # Write Visma formatted files
     def Visma_Format(self, path, data):
+        df = pd.DataFrame.from_dict(data)
+        writer = pd.ExcelWriter(path, engine='xlsxwriter', date_format='DD.MM.YYYY', datetime_format='DD.MM.YYYY') # pylint: disable=abstract-class-instantiated'
+        df.to_excel(writer, sheet_name='Fakturagrunnlag', index=False)
 
-        pass
+        # Attempt Write to File
+        try:
+            writer.save()
+            print(f'\nAll Done, file is available at {path}')
+        except:
+            print(f'\nWriting to file has failed... Oopsy')
